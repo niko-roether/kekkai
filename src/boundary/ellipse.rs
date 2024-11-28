0a61103306ae12d73137fb4dad7eea51220ba277
nicholas-roether <nicholas-roether@t-online.de>
2024-11-28 15:02:44 +0100

    Tests for ellipse boundary

--- src/boundary/ellipse.rs
+++ src/boundary/ellipse.rs
@@ -8,11 +8,11 @@ pub struct Ellipse {
 }
 
 impl Ellipse {
-    pub fn new(focus_1: Vector2<f32>, focus_2: Vector2<f32>, average_distance: f32) -> Self {
+    pub fn new(focus_1: Vector2<f32>, focus_2: Vector2<f32>, major_axis: f32) -> Self {
         Self {
             focus_1,
             focus_2,
-            average_distance,
+            average_distance: major_axis,
         }
     }
 
@@ -32,5 +32,35 @@ impl Ellipse {
     pub fn signed_distance(&self, pos: Vector2<f32>) -> f32 {
         let avg_dist = ((pos - self.focus_1).magnitude() + (pos - self.focus_2).magnitude()) / 2.0;
         avg_dist - self.average_distance
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use core::f32;
+
+    use super::*;
+
+    #[test]
+    fn should_construct_from_radii() {
+        // when
+        let ellipse = Ellipse::from_radii(vector![0.0, 1.0], 2.0, 3.0, f32::consts::TAU / 8.0);
+
+        // then
+        assert_eq!(ellipse.focus_1, vector![1.5811388, 2.5811388]);
+        assert_eq!(ellipse.focus_2, vector![-1.5811388, -0.58113885]);
+        assert_eq!(ellipse.average_distance, 3.0);
+    }
+
+    #[test]
+    fn should_return_correct_signed_distances() {
+        // when
+        let ellipse = Ellipse::new(vector![-1.0, 0.0], vector![1.0, 0.0], 2.0);
+
+        // then
+        assert_eq!(ellipse.signed_distance(vector![2.0, 0.0]), 0.0);
+        assert_eq!(ellipse.signed_distance(vector![3.0, 0.0]), 1.0);
+        assert_eq!(ellipse.signed_distance(vector![1.0, 0.0]), -1.0);
+        assert_eq!(ellipse.signed_distance(vector![2.0, 2.0]), 0.92080975);
     }
 }
