44125ad24baad4e211a934f783ed850abae7d3dd
niko-roether <nicholas.roether@t-online.de>
2024-12-02 21:32:39 +0100

    Expose consts associated to scalar type in crate::geometry

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,9 +1,16 @@
 mod vector;
 
 #[cfg(feature = "f32")]
-pub type Scalar = f32;
+pub mod scalar {
+    pub use std::f32::*;
+    pub type Scalar = f32;
+}
 
 #[cfg(not(feature = "f32"))]
-pub type Scalar = f64;
+pub mod scalar {
+    pub use std::f64::*;
+    pub type Scalar = f64;
+}
 
+pub use scalar::Scalar;
 pub use vector::*;
