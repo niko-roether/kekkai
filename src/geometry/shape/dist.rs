5d2413d2e5ed867f958f223eeed371021bbab45d
niko-roether <nicholas.roether@t-online.de>
2024-12-04 18:30:08 +0100

    Fix distance function tests

--- src/geometry/shape/dist.rs
+++ src/geometry/shape/dist.rs
@@ -35,27 +35,30 @@ mod tests {
     fn point_to_segment() {
         let p = Point::new(3.0, 2.0);
         let segment = Segment::new(Point::new(2.0, 1.0), Point::new(4.0, 2.0));
-        assert_approx_eq!(super::point_to_segment(p, &segment), 0.3826834323650898)
+        assert_approx_eq!(
+            super::point_to_segment(p, &segment),
+            1.0 / Scalar::sqrt(5.0)
+        )
     }
 
     #[test]
     fn segment_to_segment_case_1() {
         let s1 = Segment::new(Point::new(1.0, 1.0), Point::new(2.0, 3.0));
         let s2 = Segment::new(Point::new(2.0, 2.0), Point::new(4.0, 3.0));
-        assert_approx_eq!(super::segment_to_segment(&s1, &s2), 0.5);
+        assert_approx_eq!(super::segment_to_segment(&s1, &s2), 1.0 / Scalar::sqrt(5.0));
     }
 
     #[test]
     fn segment_to_segment_case_2() {
         let s1 = Segment::new(Point::new(2.0, 2.0), Point::new(4.0, 3.0));
         let s2 = Segment::new(Point::new(1.0, 1.0), Point::new(2.0, 3.0));
-        assert_approx_eq!(super::segment_to_segment(&s1, &s2), 0.5);
+        assert_approx_eq!(super::segment_to_segment(&s1, &s2), 1.0 / Scalar::sqrt(5.0));
     }
 
     #[test]
     fn segment_to_segment_parallel() {
         let s1 = Segment::new(Point::new(1.0, 1.0), Point::new(2.0, 3.0));
         let s2 = Segment::new(Point::new(2.0, 2.0), Point::new(3.0, 4.0));
-        assert_approx_eq!(super::segment_to_segment(&s1, &s2), 0.5);
+        assert_approx_eq!(super::segment_to_segment(&s1, &s2), 1.0 / Scalar::sqrt(5.0));
     }
 }
