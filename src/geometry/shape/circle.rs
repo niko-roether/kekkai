2b285f763ca86020bf7b772b7aa396835c7676c7
niko-roether <nicholas.roether@t-online.de>
2024-12-06 00:18:29 +0100

    Add a bunch of tests to Circle

--- src/geometry/shape/circle.rs
+++ src/geometry/shape/circle.rs
@@ -1,7 +1,13 @@
-use crate::geometry::{transform::Similarity, Scalar};
+use std::ops::{Mul, MulAssign};
+
+use crate::{
+    geometry::{transform::Similarity, Scalar},
+    utils::approx::ApproxEq,
+};
 
 use super::Point;
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct Circle {
     pub center: Point,
     pub radius: Scalar,
@@ -23,5 +29,67 @@ impl Circle {
     pub fn transform(&mut self, t: &Similarity) {
         self.center *= t;
         self.radius *= t.scale();
+    }
+}
+
+impl ApproxEq for Circle {
+    type Epsilon = <Scalar as ApproxEq>::Epsilon;
+
+    fn approx_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        self.center.approx_eq(&other.center, epsilon)
+            && self.radius.approx_eq(&other.radius, epsilon)
+    }
+}
+
+impl Mul<Circle> for &Similarity {
+    type Output = Circle;
+
+    fn mul(self, mut rhs: Circle) -> Self::Output {
+        rhs.transform(self);
+        rhs
+    }
+}
+
+impl MulAssign<&Similarity> for Circle {
+    fn mul_assign(&mut self, rhs: &Similarity) {
+        self.transform(rhs);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        geometry::{scalar, vector},
+        utils::approx::assert_approx_eq,
+    };
+
+    use super::*;
+
+    #[test]
+    fn signed_distance_positive() {
+        let circle = Circle::new(Point::new(1.0, 1.0), 1.0);
+        assert_approx_eq!(
+            circle.signed_distance(Point::ORIGIN),
+            scalar::consts::SQRT_2 - 1.0
+        )
+    }
+
+    #[test]
+    fn signed_distance_zero() {
+        let circle = Circle::new(Point::new(1.0, 1.0), 1.0);
+        assert_approx_eq!(circle.signed_distance(Point::new(1.0, 0.0)), 0.0)
+    }
+
+    #[test]
+    fn signed_distance_negative() {
+        let circle = Circle::new(Point::new(1.0, 1.0), 1.0);
+        assert_approx_eq!(circle.signed_distance(Point::new(1.0, 1.0)), -1.0)
+    }
+
+    #[test]
+    fn transform() {
+        let mut circle = Circle::new(Point::new(1.0, 2.0), 1.0);
+        circle *= &Similarity::from_parts(2.0, scalar::consts::TAU / 4.0, vector!(2.0, -1.0));
+        assert_approx_eq!(circle, Circle::new(Point::new(-2.0, 1.0), 2.0), 2.0);
     }
 }
